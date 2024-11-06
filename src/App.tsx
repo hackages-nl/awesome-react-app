777c44a35d6197cc6f1d1ced8c78e022f6765e0d
Davy Engone <davy@hackages.io>
2024-11-06 11:38:04 +0100

    components design

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ interface AppProps {
 }
 
 export function App({ categories, movies }: AppProps) {
-  console.log({ categories, movies });
-
   return (
     <>
       {/* Start: Header Component */}
@@ -56,24 +54,13 @@ export function App({ categories, movies }: AppProps) {
           <div className="container mx-auto text-center">
             <ul className="flex flex-row justify-center categories-list">
               {/* Start: Category */}
-              <li key={"category-1"} onClick={() => {}}>
-                <button className={"px-3 md:px-6 py-3 block"}>Category</button>
-              </li>
-              {/* End: Category */}
-              {/* Start: Category */}
-              <li key={"category-2"} onClick={() => {}}>
-                <button className={"px-3 md:px-6 py-3 block"}>Category</button>
-              </li>
-              {/* End: Category */}
-              {/* Start: Category */}
-              <li key={"category-3"} onClick={() => {}}>
-                <button className={"px-3 md:px-6 py-3 block"}>Category</button>
-              </li>
-              {/* End: Category */}
-              {/* Start: Category */}
-              <li key={"category-4"} onClick={() => {}}>
-                <button className={"px-3 md:px-6 py-3 block"}>Category</button>
-              </li>
+              {categories.map((category) => (
+                <li key={category.name} onClick={() => {}}>
+                  <button className={"px-3 md:px-6 py-3 block"}>
+                    {category.name}
+                  </button>
+                </li>
+              ))}
               {/* End: Category */}
             </ul>
           </div>
@@ -85,44 +72,16 @@ export function App({ categories, movies }: AppProps) {
           <div className="container mx-auto">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-10">
               {/* Start: Movie Component */}
-              <div key={"movie-1"} className="single-movie relative">
-                <img src="./image/poster.svg" alt={"movie title"} />
-                <div className="movie-content flex items-center justify-center text-center absolute w-full h-full inset-0 px-4">
-                  <div className="content-inner">
-                    <h3 className="mb-5">{"Movie title"}</h3>
+              {movies.map((movie) => (
+                <div key={movie.id} className="single-movie relative">
+                  <img src={movie.poster_path} alt={movie.title} />
+                  <div className="movie-content flex items-center justify-center text-center absolute w-full h-full inset-0 px-4">
+                    <div className="content-inner">
+                      <h3 className="mb-5">{movie.title}</h3>
+                    </div>
                   </div>
                 </div>
-              </div>
-              {/* End: Movie Component */}
-              {/* Start: Movie Component */}
-              <div key={"movie-2"} className="single-movie relative">
-                <img src="./image/poster.svg" alt={"movie title"} />
-                <div className="movie-content flex items-center justify-center text-center absolute w-full h-full inset-0 px-4">
-                  <div className="content-inner">
-                    <h3 className="mb-5">{"Movie title"}</h3>
-                  </div>
-                </div>
-              </div>
-              {/* End: Movie Component */}
-              {/* Start: Movie Component */}
-              <div key={"movie-3"} className="single-movie relative">
-                <img src="./image/poster.svg" alt={"movie title"} />
-                <div className="movie-content flex items-center justify-center text-center absolute w-full h-full inset-0 px-4">
-                  <div className="content-inner">
-                    <h3 className="mb-5">{"Movie title"}</h3>
-                  </div>
-                </div>
-              </div>
-              {/* End: Movie Component */}
-              {/* Start: Movie Component */}
-              <div key={"movie-4"} className="single-movie relative">
-                <img src="./image/poster.svg" alt={"movie title"} />
-                <div className="movie-content flex items-center justify-center text-center absolute w-full h-full inset-0 px-4">
-                  <div className="content-inner">
-                    <h3 className="mb-5">{"Movie title"}</h3>
-                  </div>
-                </div>
-              </div>
+              ))}
               {/* End: Movie Component */}
             </div>
           </div>
